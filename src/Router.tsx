19022ad46d5f41486f83fa67dd8b7f4869002837
hwahyeon <hwahyeon@knou.ac.kr>
2023-10-27 20:51:13 +0900

    Added Nested Routes

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Coin from "./routes/Coin";
 import Coins from "./routes/Coins";
+import Chart from "./routes/Chart";
+import Price from "./routes/Price";
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Coins />} />
-        <Route path="/:coinId" element={<Coin />} />
+        <Route path="/:coinId/*" element={<Coin />} />
       </Routes>
     </BrowserRouter>
   );
