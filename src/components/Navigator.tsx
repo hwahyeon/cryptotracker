51c5a9578639f4358f4c0d0b597f9f5416e93471
hwahyeon <hwahyeon@knou.ac.kr>
2023-11-23 23:58:47 +0900

    Update buttons

--- src/components/Navigator.tsx
+++ src/components/Navigator.tsx
@@ -1,20 +1,32 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoon, faHome } from '@fortawesome/free-solid-svg-icons';
+import { useNavigate } from 'react-router-dom';
+import { useSetRecoilState } from "recoil";
+import { isDarkAtom } from "../atoms";
 
 interface DualButtonProps {}
 
 const Navigator = (props: DualButtonProps) => {
-    const handleFirstButtonClick = () => {
-        console.log("첫 번째 버튼 클릭됨!");
+    const navi = useNavigate()
+    const setDarkAtom = useSetRecoilState(isDarkAtom);
+
+    const handleFirstButtonClick = () => {    
+        setDarkAtom(prev => !prev);
     };
 
-    const handleSecondButtonClick = () => {
-        console.log("두 번째 버튼 클릭됨!");
+    const handleHomeButtonClick = () => {
+        navi("/");
     };
 
     return (
         <div>
-            <button onClick={handleFirstButtonClick}>버튼 1</button>
-            <button onClick={handleSecondButtonClick}>버튼 2</button>
+            <button onClick={handleFirstButtonClick}>
+                <FontAwesomeIcon icon={faMoon} /> 다크 모드
+            </button>
+            <button onClick={handleHomeButtonClick}>
+                <FontAwesomeIcon icon={faHome} /> 홈
+            </button>
         </div>
     );
 }
