18b2ec192ff6911e0cfa9ea298e0aa70d7cec020
hwahyeon <hwahyeon@knou.ac.kr>
2023-11-24 01:59:54 +0900

    Update Navigator.tsx
    Added faSun icon
    Changed button names
    Added styles

--- src/components/Navigator.tsx
+++ src/components/Navigator.tsx
@@ -1,17 +1,42 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMoon, faHome } from '@fortawesome/free-solid-svg-icons';
+import { faMoon, faSun, faHome } from '@fortawesome/free-solid-svg-icons';
 import { useNavigate } from 'react-router-dom';
-import { useSetRecoilState } from "recoil";
+import { useRecoilValue, useSetRecoilState } from "recoil";
 import { isDarkAtom } from "../atoms";
+import { styled } from "styled-components";
 
-interface DualButtonProps {}
+const ButtonContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 5vh; // 전체 화면 높이
+    margin-top: 35px;
+`;
 
-const Navigator = (props: DualButtonProps) => {
+const StyledButton = styled.button`
+    background-color: rgba(0, 0, 0, 0.7); // 투명도 적용
+    color: white;
+    border: none;
+    padding: 15px;
+    margin: 0 5px;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: background-color 0.3s;
+
+    &:hover {
+        background-color: rgba(0, 0, 0, 0.9);
+    }
+`;
+
+interface NavButtonProps {}
+
+const Navigator = (props: NavButtonProps) => {
     const navi = useNavigate()
+    const isDark = useRecoilValue(isDarkAtom)
     const setDarkAtom = useSetRecoilState(isDarkAtom);
 
-    const handleFirstButtonClick = () => {    
+    const handleDarkmodeButtonClick = () => {    
         setDarkAtom(prev => !prev);
     };
 
@@ -20,14 +45,14 @@ const Navigator = (props: DualButtonProps) => {
     };
 
     return (
-        <div>
-            <button onClick={handleFirstButtonClick}>
-                <FontAwesomeIcon icon={faMoon} /> 다크 모드
-            </button>
-            <button onClick={handleHomeButtonClick}>
-                <FontAwesomeIcon icon={faHome} /> 홈
-            </button>
-        </div>
+        <ButtonContainer>
+            <StyledButton  onClick={handleDarkmodeButtonClick}>
+                <FontAwesomeIcon icon={isDark ? faMoon : faSun} />
+            </StyledButton >
+            <StyledButton  onClick={handleHomeButtonClick}>
+                <FontAwesomeIcon icon={faHome} />
+            </StyledButton >
+        </ButtonContainer>
     );
 }
 
